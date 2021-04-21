9d71346cca7379c5ed5c9f1adab79b2d210a86cf
Yrii <hvozdetskiyyrii@gmail.com>
2021-04-21 16:16:31 +0300

    improv open && close modal js

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,15 +1,34 @@
 (() => {
-    const refs = {
-        openModalBtn: document.querySelector("[data-modal-open]"),
-        closeModalBtn: document.querySelector("[data-modal-close]"),
-        modal: document.querySelector("[data-modal]"),
-    };
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+  };
 
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn.addEventListener('click', onOpenModal);
+  refs.closeModalBtn.addEventListener('click', onCloseModal);
 
-    function toggleModal() {
-        document.body.classList.toggle("modal-open"); // дод клас 'modal-open' на body щоб сторінка не прокручувувалася коли відкрита модалка
-        refs.modal.classList.toggle("is-hidden");
-    }
+  function onOpenModal() {
+    document.body.classList.add('modal-open');
+    refs.modal.classList.remove('is-hidden');
+
+    window.addEventListener('keydown', onKeydownClose);
+    refs.modal.addEventListener('click', onOverlay);
+  }
+
+  function onCloseModal() {
+    document.body.classList.remove('modal-open');
+    refs.modal.classList.add('is-hidden');
+
+    window.removeEventListener('keydown', onKeydownClose);
+    refs.modal.removeEventListener('click', onOverlay);
+  }
+
+  function onKeydownClose(e) {
+    if (e.code === 'Escape') onCloseModal();
+  }
+
+  function onOverlay(e) {
+    if (e.target === refs.modal) onCloseModal();
+  }
 })();
